e777906a1ced11da3a8fd579a242b8c891287184
jazz <jasmin.borsig@gmail.com>
2019-10-10 10:31:56 +0200

    ESDK-1676 implement integration test setup in Kotlin

---
+++ src/integTest/kotlin/de/abas/esdk/g30l0/AbstractTest.kt
@@ -0,0 +1,71 @@
+package de.abas.esdk.g30l0
+
+import de.abas.erp.db.schema.customer.Customer
+import de.abas.erp.db.schema.customer.CustomerContact
+import de.abas.erp.db.schema.customer.CustomerContactEditor
+import de.abas.erp.db.schema.customer.CustomerEditor
+import de.abas.erp.db.schema.referencetypes.TradingPartner
+import de.abas.erp.db.schema.referencetypes.TradingPartnerEditor
+import de.abas.erp.db.schema.vendor.Vendor
+import de.abas.erp.db.schema.vendor.VendorContact
+import de.abas.erp.db.schema.vendor.VendorContactEditor
+import de.abas.erp.db.schema.vendor.VendorEditor
+import de.abas.esdk.g30l0.AbstractTest.TestingData.CUSTOMER
+import de.abas.esdk.g30l0.AbstractTest.TestingData.CUSTOMER_CONTACT
+import de.abas.esdk.g30l0.AbstractTest.TestingData.INVALID
+import de.abas.esdk.g30l0.AbstractTest.TestingData.VENDOR
+import de.abas.esdk.g30l0.AbstractTest.TestingData.VENDOR_CONTACT
+import de.abas.esdk.test.util.EsdkIntegTest
+import de.abas.esdk.test.util.TestData
+import org.junit.AfterClass
+import org.junit.BeforeClass
+
+abstract class AbstractTest : EsdkIntegTest() {
+
+	companion object {
+
+		@JvmStatic
+		@BeforeClass
+		fun prepare() {
+			createTestData(CustomerEditor::class.java, CUSTOMER)
+			createTestData(CustomerContactEditor::class.java, CUSTOMER_CONTACT)
+			createTestData(VendorEditor::class.java, VENDOR)
+			createTestData(VendorContactEditor::class.java, VENDOR_CONTACT)
+			createTestData(CustomerEditor::class.java, INVALID)
+		}
+
+		private fun <T : TradingPartnerEditor> createTestData(clazz: Class<T>, testingData: TestingData) {
+			val editor = ctx.newObject(clazz)
+			if (editor is CustomerContactEditor) {
+				(editor as CustomerContactEditor).companyARAP = CUSTOMER.tradingPartner as Customer
+			}
+			if (editor is VendorContactEditor) {
+				(editor as VendorContactEditor).companyARAP = VENDOR.tradingPartner as Vendor
+			}
+			editor.swd = testingData.swd
+			editor.zipCode = testingData.zipCode
+			editor.town = testingData.town
+			editor.commit()
+			testingData.tradingPartner = editor
+		}
+
+		@JvmStatic
+		@AfterClass
+		fun cleanup() {
+			TestData.deleteData(ctx, Customer::class.java, Customer.META.swd, CUSTOMER.swd)
+			TestData.deleteData(ctx, CustomerContact::class.java, CustomerContact.META.swd, CUSTOMER_CONTACT.swd)
+			TestData.deleteData(ctx, Vendor::class.java, Vendor.META.swd, VENDOR.swd)
+			TestData.deleteData(ctx, VendorContact::class.java, VendorContact.META.swd, VENDOR_CONTACT.swd)
+			TestData.deleteData(ctx, Customer::class.java, Customer.META.swd, INVALID.swd)
+		}
+	}
+
+	enum class TestingData(val swd: String, val zipCode: String, val town: String, var tradingPartner: TradingPartner? = null) {
+		CUSTOMER("G30L0CUS", "67165", "Waldsee"),
+		VENDOR("G30L0VEN", "76135", "Karlsruhe"),
+		CUSTOMER_CONTACT("G30L0CCO", "67346", "Speyer"),
+		VENDOR_CONTACT("G30L0VCO", "76227", "Karlsruhe"),
+		INVALID("G30L0INV", "invalid", "invalid")
+	}
+
+}
