e8b40889b28ce498fa259ea638b2d31f5db46c16
Rainer Schmitz <Rainer.Schmitz@abas.de>
2019-10-11 13:40:06 +0200

    Refactoring: - Geolocation now is a immutable data class. - OpenStreetMapGeolocationResolver is more Kotlin idiomatic.

--- src/main/kotlin/de/abas/esdk/g30l0/Geolocation.kt
+++ src/main/kotlin/de/abas/esdk/g30l0/Geolocation.kt
@@ -1,6 +1,3 @@
 package de.abas.esdk.g30l0
 
-class Geolocation {
-	var latitude = ""
-	var longitude = ""
-}
+data class Geolocation(val latitude: String = "", val longitude: String = "")
--- src/main/kotlin/de/abas/esdk/g30l0/OpenStreetMapGeolocationResolver.kt
+++ src/main/kotlin/de/abas/esdk/g30l0/OpenStreetMapGeolocationResolver.kt
@@ -7,32 +7,32 @@ import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.log4j.Logger
 import java.io.IOException
 
-
 class OpenStreetMapGeolocationResolver : GeolocationResolver {
-	private val logger: Logger = Logger.getLogger(OpenStreetMapGeolocationResolver::class.java)
+    private val logger: Logger = Logger.getLogger(OpenStreetMapGeolocationResolver::class.java)
 
-	override fun resolve(tradingPartner: TradingPartner): Geolocation {
-		val geolocation = Geolocation()
-		try {
-			val addresses = resolveFromOpenStreetMaps(tradingPartner)
-			if (addresses.isEmpty()) {
-				logger.debug("No matches found for address ${getFormattedAddress(tradingPartner)}")
-			} else {
-				geolocation.latitude = addresses[0].latitude.toString()
-				geolocation.longitude = addresses[0].longitude.toString()
-			}
-		} catch (e: IOException) {
-			logger.error("Invalid address '${getFormattedAddress(tradingPartner)}': ${e.message}", e)
-		}
-		return geolocation
-	}
+    override fun resolve(tradingPartner: TradingPartner): Geolocation {
+        val addresses = try {
+            resolveFromOpenStreetMaps(tradingPartner)
+        } catch (e: IOException) {
+            logger.error("Invalid address '${getFormattedAddress(tradingPartner)}': ${e.message}", e)
+            return Geolocation()
+        }
 
-	private fun getFormattedAddress(tradingPartner: TradingPartner) = "${tradingPartner.street}, ${tradingPartner.zipCode} ${tradingPartner.town}, ${tradingPartner.stateOfTaxOffice.swd}"
+        return addresses.firstOrNull()?.run {
+            Geolocation(latitude.toString(), longitude.toString())
+        } ?: Geolocation().also {
+            logger.debug("No matches found for address ${getFormattedAddress(tradingPartner)}")
+        }
+    }
 
-	@Throws(IOException::class)
-	private fun resolveFromOpenStreetMaps(tradingPartner: TradingPartner): List<Address> {
-		val jsonNominatimClient = JsonNominatimClient(DefaultHttpClient(), "scrumteamesdk@abas.de")
-		return jsonNominatimClient.search("${tradingPartner.street} ${tradingPartner.zipCode} ${tradingPartner.town} ${tradingPartner.stateOfTaxOffice.swd}")
-	}
+    private fun getFormattedAddress(tradingPartner: TradingPartner) = "${tradingPartner.street}, ${tradingPartner.zipCode} ${tradingPartner.town}, ${tradingPartner.stateOfTaxOffice.swd}"
+
+    @Throws(IOException::class)
+    private fun resolveFromOpenStreetMaps(tradingPartner: TradingPartner): List<Address> {
+        val jsonNominatimClient = JsonNominatimClient(DefaultHttpClient(), "scrumteamesdk@abas.de")
+        with(tradingPartner) {
+            return jsonNominatimClient.search("$street $zipCode $town ${stateOfTaxOffice.swd}")
+        }
+    }
 
 }
