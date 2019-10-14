ee47eea64526da013541cc72788bcfe03a6ca442
Rainer Schmitz <Rainer.Schmitz@abas.de>
2019-10-14 15:30:56 +0200

    Use lambda with receiver to simplify query DSL.

--- src/main/kotlin/de/abas/esdk/g30l0/TradingPartnerSelector.kt
+++ src/main/kotlin/de/abas/esdk/g30l0/TradingPartnerSelector.kt
@@ -2,22 +2,21 @@ package de.abas.esdk.g30l0
 
 import de.abas.erp.db.DbContext
 import de.abas.erp.db.FieldValueProvider
+import de.abas.erp.db.SelectableRecord
 import de.abas.erp.db.field.StringField
 import de.abas.erp.db.schema.customer.Customer
 import de.abas.erp.db.schema.customer.CustomerContact
 import de.abas.erp.db.schema.referencetypes.TradingPartner
 import de.abas.erp.db.schema.vendor.Vendor
 import de.abas.erp.db.schema.vendor.VendorContact
+import de.abas.erp.db.selection.Condition
 import de.abas.erp.db.selection.Conditions
 import de.abas.erp.db.selection.SelectionBuilder
 import kotlin.reflect.KClass
-import de.abas.erp.db.SelectableRecord
-import de.abas.erp.db.selection.Condition
 
 object TradingPartnerSelector {
 
 	private infix fun <O : FieldValueProvider> StringField<O>.eq(value: CharSequence): Condition<O> = Conditions.eq(this, value)
-//	private fun <T : SelectableRecord> SelectionBuilder<T>.or(): SelectionBuilder<T> = this.setTermConjunction(SelectionBuilder.Conjunction.OR)
 
 	private fun <T : SelectableRecord> SelectionBuilder<T>.or(vararg conditions: Condition<in T>): SelectionBuilder<T> {
 		setTermConjunction(SelectionBuilder.Conjunction.OR)
@@ -31,14 +30,15 @@ object TradingPartnerSelector {
 		}
 	}
 
+	private fun <T : SelectableRecord> DbContext.query(clazz: Class<T>, builderAction: SelectionBuilder<T>.() -> SelectionBuilder<T>): List<T> {
+		val selection = SelectionBuilder.create(clazz).builderAction().build()
+		return createQuery(selection).execute()
+	}
+
 	fun selectTradingPartners(ctx: DbContext, swd: String, zipCode: String): List<TradingPartner> {
 
-		fun <T : TradingPartner> selectFromTradingPartner(clazz: Class<T>): List<T> {
-			val selection = SelectionBuilder.create(clazz)
-					.or(TradingPartner.META.swd eq swd, TradingPartner.META.zipCode eq zipCode)
-					.build()
-			return ctx.createQuery(selection)
-					.execute()
+		fun <T : TradingPartner> selectFromTradingPartner(clazz: Class<T>): List<T> = ctx.query(clazz) {
+			or(TradingPartner.META.swd eq swd, TradingPartner.META.zipCode eq zipCode)
 		}
 
 		return listOf<KClass<out TradingPartner>>(Customer::class, CustomerContact::class, Vendor::class, VendorContact::class)
