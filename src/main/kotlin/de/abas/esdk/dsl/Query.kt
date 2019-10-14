c845272843072199f89d1af6ed4cc4a398a046de
Rainer Schmitz <Rainer.Schmitz@abas.de>
2019-10-14 16:05:17 +0200

    Moved DSL into its own package.

---
+++ src/main/kotlin/de/abas/esdk/dsl/Query.kt
@@ -0,0 +1,34 @@
+package de.abas.esdk.dsl
+
+import de.abas.erp.db.DbContext
+import de.abas.erp.db.FieldValueProvider
+import de.abas.erp.db.SelectableRecord
+import de.abas.erp.db.field.StringField
+import de.abas.erp.db.selection.Condition
+import de.abas.erp.db.selection.Conditions
+import de.abas.erp.db.selection.SelectionBuilder
+
+/**
+ * Skeleton of a DSL to build selections and execute queries.
+ *
+ * This is tailored to be used by [TradingPartnerSelector] but it could be developed into a universal DSL.
+ */
+
+fun <T : SelectableRecord> DbContext.query(clazz: Class<T>, builderAction: SelectionBuilder<T>.() -> SelectionBuilder<T>): List<T> {
+	val selection = SelectionBuilder.create(clazz).builderAction().build()
+	return createQuery(selection).execute()
+}
+
+infix fun <O : FieldValueProvider> StringField<O>.eq(value: CharSequence): Condition<O> = Conditions.eq(this, value)
+
+fun <T : SelectableRecord> SelectionBuilder<T>.or(vararg conditions: Condition<in T>): SelectionBuilder<T> {
+	setTermConjunction(SelectionBuilder.Conjunction.OR)
+	addConditions(conditions)
+	return this
+}
+
+private fun <T : SelectableRecord> SelectionBuilder<T>.addConditions(conditions: Array<out Condition<in T>>) {
+	conditions.toList().forEach { condition ->
+		add(condition)
+	}
+}
--- src/main/kotlin/de/abas/esdk/g30l0/TradingPartnerSelector.kt
+++ src/main/kotlin/de/abas/esdk/g30l0/TradingPartnerSelector.kt
@@ -1,39 +1,17 @@
 package de.abas.esdk.g30l0
 
 import de.abas.erp.db.DbContext
-import de.abas.erp.db.FieldValueProvider
-import de.abas.erp.db.SelectableRecord
-import de.abas.erp.db.field.StringField
 import de.abas.erp.db.schema.customer.Customer
 import de.abas.erp.db.schema.customer.CustomerContact
 import de.abas.erp.db.schema.referencetypes.TradingPartner
 import de.abas.erp.db.schema.vendor.Vendor
 import de.abas.erp.db.schema.vendor.VendorContact
-import de.abas.erp.db.selection.Condition
-import de.abas.erp.db.selection.Conditions
-import de.abas.erp.db.selection.SelectionBuilder
+import de.abas.esdk.dsl.eq
+import de.abas.esdk.dsl.or
+import de.abas.esdk.dsl.query
 import kotlin.reflect.KClass
 
 object TradingPartnerSelector {
-
-	private infix fun <O : FieldValueProvider> StringField<O>.eq(value: CharSequence): Condition<O> = Conditions.eq(this, value)
-
-	private fun <T : SelectableRecord> SelectionBuilder<T>.or(vararg conditions: Condition<in T>): SelectionBuilder<T> {
-		setTermConjunction(SelectionBuilder.Conjunction.OR)
-		addConditions(conditions)
-		return this
-	}
-
-	private fun <T : SelectableRecord> SelectionBuilder<T>.addConditions(conditions: Array<out Condition<in T>>) {
-		conditions.toList().forEach { condition ->
-			add(condition)
-		}
-	}
-
-	private fun <T : SelectableRecord> DbContext.query(clazz: Class<T>, builderAction: SelectionBuilder<T>.() -> SelectionBuilder<T>): List<T> {
-		val selection = SelectionBuilder.create(clazz).builderAction().build()
-		return createQuery(selection).execute()
-	}
 
 	fun selectTradingPartners(ctx: DbContext, swd: String, zipCode: String): List<TradingPartner> {
 
